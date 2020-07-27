0e73549944940f3b0058368dba90456e0c51e7b5
Pietro <preggian@stern.nyu.edu>
2020-07-27 18:28:12 -0400

    Made some progress in data download

---
+++ code/funs_warehouse.r
@@ -0,0 +1,9 @@
+#' ---
+#' title: "Functions Warehouse"
+#' author: "Pietro Reggiani"
+#' date: "July 2020"
+#' output: github_document
+#' ---
+#'
+#' This file stores my user defined functions for the project. It should be called at the beginning
+#' of all scripts using the `source(funs_warehouse.r)` function.
--- code/prova.r
+++ code/prova.r
@@ -1,23 +1,25 @@
 #' ---
-#' title: "First Data Analysis Attempt"
+#' title: "Data Download and Merge"
 #' author: "Pietro Reggiani"
 #' date: "July 2020"
 #' output: github_document
 #' ---
 #'
-#' Here's some prose in a very special comment. Let's summarize the built-in
-#' dataset `VADeaths`.
+#' This file connects to WRDS and downloads the following data sets:
+#' 
+#' * Thomson Reuters S34 holdings data
+#' * Compustat quarterly file
+#' * CRSP monthly stock file
+#' * CRSP-Compustat link table
+#' 
+#' Then, the code merges these data sources to add stock level information to the holdings.
+#' The **output** of the code is 
 
-## here is a regular code comment, that will remain as such
-
-#' Here's some more prose. I can use usual markdown syntax to make things
-#' **bold** or *italics*. Let's use an example from the `dotchart()` help to
-#' make a Cleveland dot plot from the `VADeaths` data. 
-#' You can name chunks of code as in a markdown file, using #+. This is especially useful when
-#' naming figures.
+#'
+#' ## Load packages and connect to WRDS.
+#' 
 #+ Setup
 
-# First load all libraries we need
 library(data.table)
 setDTthreads(threads = 0)  #tells to use all available cores
 
@@ -25,43 +27,110 @@ setDTthreads(threads = 0)  #tells to use all available cores
 library(RPostgres)
 wrds <- dbConnect(Postgres(),
                   host='wrds-pgdata.wharton.upenn.edu',
-                  port=9737,
+                      port=9737,
                   dbname='wrds',
                   sslmode='require',
                   user='preggian')
 
-#' This code spits out all the available libraries in WRDS.
+#' ## Load Data from WRDS
+#' Here we dowload the list of all available libraries from WRDS
+
 res <- dbSendQuery(wrds, "select distinct table_schema
-                   from information_schema.tables where table_type ='VIEW'
+                   from information_schema.tables
+                   where table_type ='VIEW'
+                   or table_type ='FOREIGN TABLE'
                    order by table_schema")
 libraries <- dbFetch(res, n=-1)
 dbClearResult(res)
 
-#' Now you can ask for information about one specific library. In our case it's the Thomson
-#' Reuters one.
+
+
+#' ### Thomson Reuters S34
 #' 
+#' Here we first input the holdings data from Thomson Reuters
+
+res <- dbSendQuery(wrds, "select * from tfn.s34 where fdate between '2019-12-31' and '2020-03-31'")
+s34 <- as.data.table(dbFetch(res, n=1000))  #use data.table package
+dbClearResult(res)
+
+s34 <- s34[order(mgrname,cusip, -fdate)] #order by manager, stock and date  
+
+#' ### Compustat
+#' This part considers the Compustat file.
+#' 
+
 res <- dbSendQuery(wrds, "select distinct table_name
                    from information_schema.columns
-                   where table_schema='tfn'
+                   where table_schema='comp'
                    order by table_name")
-tables <- dbFetch(res, n=-1)
+comp.libraries <- dbFetch(res, n=-1)  
 dbClearResult(res)
-View(tables)
 
-#' This code instead spits out all the variables relative to a specific table. In 
-#' this case let us print the variables that are in the main S34 table.
+#' comp.fundq is the Fundamentals Quarterly dataset that we are interested in.
+#' Now let's print the variable names available
+#' These are all the 647 variables available, 
+#' but in principle you may want to extract only a portion.
 
 res <- dbSendQuery(wrds, "select column_name
                    from information_schema.columns
-                   where table_schema='tfn'
-                   and table_name='s34'
+                   where table_schema='comp'
+                   and table_name='fundq'
                    order by column_name")
-vars <- dbFetch(res, n=-1)
+comp.vars <- dbFetch(res, n=-1)  
 dbClearResult(res)
-View(vars)
 
 
-#' Ok so now that we have looked at the variables, we are ready to query the actual data.
-res <- dbSendQuery(wrds, "select * from tfn.s34 where fdate >='2020-03-31'")
-data <- as.data.table(dbFetch(res, n=100))  #use data.table package
+#' Now we are ready to query the Compustat data. Remember that each firm is identified by the gvkey
+#' identifier.
+res <- dbSendQuery(wrds, "select * from comp.fundq where fyearq = 2020")
+comp.data <- as.data.table(dbFetch(res, n=1000))  #use data.table package
 dbClearResult(res)
+
+
+#' ### CRSP Stock Data
+#' 
+res <- dbSendQuery(wrds, "select distinct table_name
+                   from information_schema.columns
+                   where table_schema='crspq'
+                   order by table_name")
+crsp.libraries <- dbFetch(res, n=-1)  
+dbClearResult(res)
+
+#' We will use stock level data from the monthly stock file `msf`.
+
+res <- dbSendQuery(wrds, "select column_name
+                   from information_schema.columns
+                   where table_schema='crsp'
+                   and table_name='msf'
+                   order by column_name")
+crsp.vars <- dbFetch(res, n=-1)  
+dbClearResult(res)
+
+
+#' Now we are ready to query the data.
+
+res <- dbSendQuery(wrds, "select * from crsp.msf where date >= '2019-12-31'")
+crsp.data <- as.data.table(dbFetch(res, n=1000))  #use data.table package
+dbClearResult(res)
+
+ 
+#' ### CRSP-Compustat link file 
+#' We want to match the different datasets, let's get also the CRSP-COMPUSTAT link.
+
+res <- dbSendQuery(wrds, "select column_name
+                   from information_schema.columns
+                   where table_schema='crspq'
+                   and table_name='ccmxpf_lnkhist'
+                   order by column_name")
+ccm.vars <- dbFetch(res, n=-1)  
+dbClearResult(res)
+
+
+res <- dbSendQuery(wrds, "select * from crsp.ccmxpf_lnkhist")
+ccm <- as.data.table(dbFetch(res, n=10000))  #use data.table package
+dbClearResult(res)
+
+ccm <- ccm[order(gvkey, linkdt)]
+
+
+
